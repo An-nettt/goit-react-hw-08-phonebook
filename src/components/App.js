76a4d4c03baad5c6b92323d2d4a30371f564a08f
Anna Kravets <annakravets1906@gmail.com>
2023-07-01 12:21:38 +0300

    Added React.lazy та React.Suspense

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,21 @@
-import { useEffect } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './Theme/Theme';
 
-import Home from 'pages/HomePage';
-import LoginPage from 'pages/LoginPage';
-import RegisterPage from 'pages/RegisterPage';
-import Contacts from 'pages/ContactsPage';
-// import NotFound from 'pages/NotFound';
-
+import Header from './Header/AppBar';
 import { PrivateRoute } from './Header/PrivateRoute';
 import { RestrictedRoute } from './Header/RestrictedRoute';
 
 import { getCurrentUser } from 'redux/auth/authThunks';
-import Header from './Header/AppBar';
-import NotFound from 'pages/NotFoundPage';
+import Loader from './Loader/Loader';
+
+const Home = lazy(() => import('pages/HomePage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
+const Contacts = lazy(() => import('pages/ContactsPage'));
+const NotFound = lazy(() => import('pages/NotFoundPage'));
 
 export default function App() {
   const dispatch = useDispatch();
@@ -28,32 +27,35 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                element={<RegisterPage />}
+                redirectTo="/contacts"
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute element={<LoginPage />} redirectTo="/contacts" />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute element={<Contacts />} redirectTo="/login" />
+            }
+          />
 
-      <Routes>
-        <Route path="/goit-react-hw-08-phonebook/" element={<Home />} />
-        <Route index element={<Home />} />
-        <Route
-          path="/register"
-          element={
-            <RestrictedRoute
-              element={<RegisterPage />}
-              redirectTo="/contacts"
-            />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <RestrictedRoute element={<LoginPage />} redirectTo="/contacts" />
-          }
-        />
-        <Route
-          path="/contacts"
-          element={<PrivateRoute element={<Contacts />} redirectTo="/login" />}
-        />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </ThemeProvider>
   );
 }
